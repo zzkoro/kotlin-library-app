aff03ac17f9bb1dd2367e1881cc8a7643d19e49e
junemp <junemp@gmail.com>
2022-11-08 08:23:41 +0900

    UserLoanHistory 리팩토링

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -16,6 +16,9 @@ class UserLoanHistory (
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 ) {
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
