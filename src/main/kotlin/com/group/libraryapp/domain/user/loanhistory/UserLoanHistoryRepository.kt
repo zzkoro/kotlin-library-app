20432621547dc2f80aa2fea72e388088ecf17d6b
junemp <junemp@gmail.com>
2022-11-12 21:53:50 +0900

    Querydsl 완료

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
 
+    fun findByBookName(bookName: String): UserLoanHistory?
+
     fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?;
 
     fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -10,6 +10,8 @@ import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQuerydslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -18,7 +20,9 @@ import org.springframework.transaction.annotation.Transactional
 class BookService (
     private val bookRepository: BookRepository,
     private val userRepository: UserRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryRepository
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+    private val bookQuerydslRepository: BookQuerydslRepository,
+    private val userLoanHistoryQuerydslRepository: UserLoanHistoryQuerydslRepository,
     ) {
 
     @Transactional
@@ -30,7 +34,7 @@ class BookService (
     @Transactional
     fun loanBook(request: BookLoanRequest) {
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if (userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null) {
+        if (userLoanHistoryQuerydslRepository.find(request.bookName, UserLoanStatus.LOANED) != null) {
             throw IllegalArgumentException("진작 대출되어 있는 책입니다.")
         }
 
@@ -53,7 +57,9 @@ class BookService (
     @Transactional(readOnly = true)
     fun getBookStatistics(): List<BookStatResponse> {
 
-        return bookRepository.getStats()
+//        return bookRepository.getStats()
+
+        return bookQuerydslRepository.getStats()
 
 //        return bookRepository.findAll()
 //            .groupBy { book -> book.type } // Map<BookType, List<Book>>
